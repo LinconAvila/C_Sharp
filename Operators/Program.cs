c1dbba78aad38f26d1f576505e7953f516b6895f
Lincon Avila <linconavila.souza@gmail.com>
2024-11-12 19:26:02 -0300

    Add a operators exercise

---
+++ Operators/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 23;
+
+            age++;
+            age += 1;
+
+            Console.WriteLine(age);
+            
+            age--;
+            Console.WriteLine(age);
+
+            string name = "Aba";
+            name += " is programming!";
+
+            Console.WriteLine(name);
+
+
+  
+            Console.ReadLine();
+        }
+    }
+}
